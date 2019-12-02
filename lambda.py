9c43fa859e39e4b789942d383668b40d906529ce
Marcelo Martins <727100@gmail.com>
2019-12-01 23:29:48 -0300

    lambda

---
+++ lambda.py
@@ -0,0 +1,41 @@
+x = lambda a : a + 10
+print(x(5))
+
+print('\n')
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+print('\n')
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+print('\n')
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+print('\n')
+
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
+print('\n')
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
