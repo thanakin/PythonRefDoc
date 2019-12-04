05075b9e5cfa796eeb8ca75b7e373f7aebc6d5bf
Marcelo Martins <727100@gmail.com>
2019-12-03 23:58:43 -0300

    user input

---
+++ userinput.py
@@ -0,0 +1,9 @@
+'''
+#Python 2.7
+username = raw_input("Enter username:")
+print("Username is: " + username)
+'''
+
+#Python 3.6
+username = input("Enter username: ")
+print("Username is: " + username)
