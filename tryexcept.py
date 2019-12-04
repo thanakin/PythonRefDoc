5d357a09b9b07973c508e270066f91028e8e37e0
Marcelo Martins <727100@gmail.com>
2019-12-03 23:56:54 -0300

    try except

---
+++ tryexcept.py
@@ -0,0 +1,52 @@
+try:
+  print(x)
+except:
+  print("An exception occurred")
+
+
+print('\nMany Exceptions')
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+
+print('\nElse')
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+
+
+print('\nFinally')
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+
+
+try:
+  f = open("demofile.txt")
+  f.write("Lorum Ipsum")
+except:
+  print("Something went wrong when writing to the file")
+finally:
+  f.close()
+
+'''
+print('\n Raise an exception')
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+
+
+x = "hello"
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
+'''
