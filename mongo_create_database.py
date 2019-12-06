0aefc3b804307a0d9eaca3a79669b1ac8d408ce6
Marcelo Martins <727100@gmail.com>
2019-12-05 23:55:53 -0300

    mongo create database

---
+++ mongo_create_database.py
@@ -0,0 +1,12 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+
+
+print('\nCheck if Database Exists')
+print(myclient.list_database_names())
+
+dblist = myclient.list_database_names()
+if "mydatabase" in dblist:
+  print("The database exists.")
