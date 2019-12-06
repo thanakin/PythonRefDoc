800e0f5bde1eebfea3aeb7964f64f56b937996aa
Marcelo Martins <727100@gmail.com>
2019-12-05 23:58:38 -0300

    mongo create collection

---
+++ mongo_create_collection.py
@@ -0,0 +1,13 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+print('\n Check if Collection Exists')
+print(mydb.list_collection_names())
+
+
+collist = mydb.list_collection_names()
+if "customers" in collist:
+  print("The collection exists.")
